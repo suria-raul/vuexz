19b744bcdd0fcfe9f7ac6ec7fdd6b2bf41479f97
vava_raul <stadnitzki.nero.nero@gmail.com>
2023-08-18 16:39:59 +0800

    The net ninja vuex tutorial - stopped at tutorial #7

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <product-list-one/>
+    <product-list-two/>
+  </div>
+</template>
+
+<script>
+import ProductListOne from "./components/ProductListOne.vue";
+import ProductListTwo from "./components/ProductListTwo.vue";
+
+export default {
+  name: 'app',
+  components: {ProductListTwo, ProductListOne},
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
---
+++ src/store/store.js
@@ -0,0 +1,36 @@
+import Vue from "vue"
+import Vuex from "vuex"
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store(
+  {
+    state: {
+      products: [
+        {name: 'Banana Skin', price: 20},
+        {name: 'Shiny Star', price: 40},
+        {name: 'Green Shells', price: 60},
+        {name: 'Red Shells', price: 80},
+      ]
+    },
+    getters: {
+      saleProducts: state => {
+        return state.products.map(product => {
+          return {
+            name: '**' + product.name + '**',
+            price: product.price / 2,
+          }
+        })
+      }
+    },
+    mutations: {
+      reducePrice: state => {
+        state.products.forEach(product => {
+          product.price -= 1
+        })
+      }
+    }
+  }
+)
+
+export default store
